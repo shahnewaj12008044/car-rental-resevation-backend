4128e0a1c8b5e528f99abb8d623fc8dd9668f434
Md Shahnewaj Al Hasan <mdshahnewajalhasan@gmail.com>
2024-09-07 11:02:16 +0600

    validation for car delete done

--- src/app/Components/Car/car.model.ts
+++ src/app/Components/Car/car.model.ts
@@ -1,5 +1,8 @@
 import { model, Schema } from "mongoose";
 import { TCar } from "./car.interface";
+import AppError from "../../Error/AppError";
+import httpStatus from "http-status-codes";
+
 
 const carShema = new Schema<TCar>(
   {
@@ -18,5 +21,29 @@ const carShema = new Schema<TCar>(
   },
   { timestamps: true }
 );
+
+//removing deleted cars from find
+carShema.pre('find',function(next){
+  this.find({isDeleted:{$ne:true}})
+  next()
+})
+// //removing deleted car from findOne
+// carShema.pre('findOne',function(next){
+//   this.find({isDeleted:{$ne:true}})
+//   next()
+// })
+
+//preventing delete a car which is already deleted:
+carShema.pre('findOneAndUpdate',async function(next) {
+  const query = this.getQuery();
+  // console.log(query)
+  const isCar = await this.model.findOne(query);
+  // console.log(isCar)
+  if(isCar || isCar?.isDeleted){
+    throw new AppError(httpStatus.FORBIDDEN, "This car is already deleted or doesn't exist!")
+  }
+  next()
+})
+
 
 export const Car = model<TCar>("Car", carShema);
